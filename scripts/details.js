219f76747b07896a913feb3ffb65d14d32269bd2
orjon <github@orjon.com>
2019-06-05 01:16:56 +0100

    Before nav rebuild

--- scripts/details.js
+++ scripts/details.js
@@ -4,6 +4,30 @@ $(() => {
   const fadeIn= 500
   const animSpeed = 3000
 
+  const currentPage = document.querySelector('.navText').innerHTML
+  const navIcons = document.querySelectorAll('.navIcon')
+  const navText = document.querySelector('.navText')
+
+  navIcons.forEach(icon => {
+    icon.addEventListener('mouseover', (e) => {
+      navText.innerHTML=''
+      if ($(e.target).hasClass('code')) {
+        navText.innerHTML='code'
+      } else if ($(e.target).hasClass('design')) {
+        navText.innerHTML='design & visualisation'
+      } else if ($(e.target).hasClass('electronics')) {
+        navText.innerHTML='electronics'
+      } else if ($(e.target).hasClass('face')) {
+        navText.innerHTML='about me'
+      }
+    })
+
+    icon.addEventListener('mouseout', () => {
+      navText.innerHTML= currentPage
+    })
+  })
+
+
   $('.explain > div:gt(0)').hide()
 
 
